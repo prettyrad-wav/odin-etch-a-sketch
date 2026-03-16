3afdaebe53c4cb0f315152e9f5c2fa623f1f5997
Nick Hobbs <officialkinggnarles@gmail.com>
2026-03-16 02:26:03 -0400

    Creat grid reset and resizing functionality

--- javascript.js
+++ javascript.js
@@ -1,44 +1,61 @@
 let body = document.querySelector("body")
-
 let gridContainer = document.querySelector(".grid-container")
+let gridInput = document.querySelector(".grid-value")
+let resetBttn = document.querySelector(".reset-bttn")
 
-let gridSize = 16
 
 function createGrid(){
-let i = 1
-let boxes = gridSize
-while (i <= gridSize) {
-let row = document.createElement("div")
-row.classList.add("row")
-gridContainer.appendChild(row)
-    for (boxes; boxes >= 1; boxes--) {
-        let box = document.createElement("div")
-        box.classList.add("box")
-        row.appendChild(box)   
-    }    
-i++
-boxes = gridSize
+    if (gridInput.value < 1 || gridInput.value > 100) {
+      alert("Invalid grid size! Input needs to be between 1-100")  
+    } else {
+        let i = 1
+        let boxes = gridInput.value
+        while (i <= gridInput.value) {
+        let row = document.createElement("div")
+        row.classList.add("row")
+        gridContainer.appendChild(row)
+            for (boxes; boxes >= 1; boxes--) {
+                let box = document.createElement("div")
+                box.classList.add("box")
+                row.appendChild(box)   
+            }    
+        i++
+        boxes = gridInput.value
+        }
+        console.log("gridcreated")
+    }
 }
+
+function clearGrid(){
+    if (gridInput.value < 1 || gridInput.value > 100) {
+        return;
+    } else {
+    let boxDelete = document.querySelectorAll(".box")
+    let rowDelete = document.querySelectorAll(".row")
+    boxDelete.forEach(el => el.remove())
+    rowDelete.forEach(el => el.remove())
+    console.log("clear grid")
+    }
 }
 
 createGrid()
 
+resetBttn.addEventListener("click", clearGrid)
+resetBttn.addEventListener("click", createGrid)
+
 function draw(e){
     if (e.target.matches('.box')) {
-                e.target.classList.toggle("color", true)
-            console.log("mouseover")    
+                e.target.classList.toggle("color", true)    
             }
 }
 
 gridContainer.addEventListener("mousedown", (e) => {
-        gridContainer.addEventListener("mouseover", draw)   
-   console.log("mousedown") 
+        gridContainer.addEventListener("mouseover", draw)    
 })
 
 
 gridContainer.addEventListener("mouseup", (e) => {
-    gridContainer.removeEventListener("mouseover", draw)
-    console.log("mouseup")    
+    gridContainer.removeEventListener("mouseover", draw)    
 })
 
 
