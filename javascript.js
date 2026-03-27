4be345673daca0b588f041ea0112eff577f0c905
Nick Hobbs <officialkinggnarles@gmail.com>
2026-03-27 00:34:12 -0400

    Create gay mode function
    created gay mode so that when the box is checked
    zestydraw function will run and each box drawn
    over will be a random rainbow color. If gay mode
    is not checked and it is june, a pop up appears
    and you can not draw.

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ let gridContainer = document.querySelector(".grid-container")
 let gridInput = document.querySelector(".grid-value")
 let resetBttn = document.querySelector(".reset-bttn")
 let gayMode = document.querySelector(".gay-mode")
-
+let date = new Date().getMonth();
 function createGrid(){
     if (gridInput.value < 1 || gridInput.value > 100) {
       alert("Invalid grid size! Input needs to be between 1-100")  
@@ -50,24 +50,23 @@ function draw(e){
 }
 
 function zestyDraw(e){
-        console.log("zestyWork?")
-
     if (e.target.matches('.box')) {
         let num = Math.floor(Math.random() * (7 - 1 + 1)) + 1;
-        if (num === 1) {
+        console.log(num);
+        if (num === 1 && !e.target.matches('.red') && !e.target.matches('.orange') && !e.target.matches('.yellow') && !e.target.matches('.green') && !e.target.matches('.blue') && !e.target.matches('.indigo') && !e.target.matches('.violet')) {
             console.log("zest")
             e.target.classList.toggle("red", true)
-        } else if (num === 2) {
+        } else if (num === 2 && !e.target.matches('.red') && !e.target.matches('.orange') && !e.target.matches('.yellow') && !e.target.matches('.green') && !e.target.matches('.blue') && !e.target.matches('.indigo') && !e.target.matches('.violet')) {
             e.target.classList.toggle("orange", true)
-        } else if (num === 3) {
+        } else if (num === 3 && !e.target.matches('.red') && !e.target.matches('.orange') && !e.target.matches('.yellow') && !e.target.matches('.green') && !e.target.matches('.blue') && !e.target.matches('.indigo') && !e.target.matches('.violet')) {
             e.target.classList.toggle("Yellow", true)
-        } else if (num === 4) {
+        } else if (num === 4 && !e.target.matches('.red') && !e.target.matches('.orange') && !e.target.matches('.yellow') && !e.target.matches('.green') && !e.target.matches('.blue') && !e.target.matches('.indigo') && !e.target.matches('.violet')) {
             e.target.classList.toggle("green", true)
-        } else if (num === 5) {
+        } else if (num === 5 && !e.target.matches('.red') && !e.target.matches('.orange') && !e.target.matches('.yellow') && !e.target.matches('.green') && !e.target.matches('.blue') && !e.target.matches('.indigo') && !e.target.matches('.violet')) {
             e.target.classList.toggle("blue", true)
-        } else if (num === 6) {
+        } else if (num === 6 && !e.target.matches('.red') && !e.target.matches('.orange') && !e.target.matches('.yellow') && !e.target.matches('.green') && !e.target.matches('.blue') && !e.target.matches('.indigo') && !e.target.matches('.violet')) {
             e.target.classList.toggle("indigo", true)
-        } else if (num === 7) {
+        } else if (num === 7 && !e.target.matches('.red') && !e.target.matches('.orange') && !e.target.matches('.yellow') && !e.target.matches('.green') && !e.target.matches('.blue') && !e.target.matches('.indigo') && !e.target.matches('.violet')) {
             e.target.classList.toggle("violet", true)
         }
         console.log("zestydraw")
@@ -76,7 +75,9 @@ function zestyDraw(e){
 
 gridContainer.addEventListener("mousedown", (e) => {
     console.log("mousedown")
-    if (gayMode.checked === true) {
+    if (gayMode.checked === false && date === 5) {
+        alert("HOMOPHOBE! HOMOPHOBE! Turn on gay mode to use this during pride month!")
+    } else if (gayMode.checked === true) {
         gridContainer.addEventListener("mouseover", zestyDraw);
     } else {
         gridContainer.addEventListener("mouseover", draw) ;  
